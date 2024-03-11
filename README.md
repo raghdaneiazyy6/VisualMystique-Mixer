8e1ab2836c5f69e03e0ac009a769a221f43bd33b
Raghda Tarek <96526181+raghdaneiazyy6@users.noreply.github.com>
2024-03-11 11:41:42 +0200

    added UI gif demo

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Dive into the mesmerizing world of VisualMystique Mixer, where we blend signal-p
 
 VisualMystique Mixer lets you unravel the mysteries of signal components, providing a unique peek into how diverse frequencies shape the overall signal. Whether you're a curious student diving into signal processing or a seasoned pro working with images, this app offers a versatile playground for experimentation.
 
+## Preview
+![Animation Gif](Demo.gif)
 
 ## Features
 
@@ -34,40 +36,19 @@ VisualMystique Mixer lets you unravel the mysteries of signal components, provid
   The mixing process involves an ifft groove, and a progress bar keeps you in the loop. Interrupt the beat with a new mixing request? No problem, we cancel the previous operation intelligently.
 
 
-
-## Getting Started
-
-To embark on your image mixing journey with VisualMystique Mixer, follow the steps outlined below:
-
-
-1. Install the application on your desktop.
-2. Launch the application and become acquainted with the user interface.
-3. Open and view grayscale images in the designated viewports.
-4. Experiment with adjusting brightness/contrast for image visualization.
-5. Analyze Fourier Transform components (Magnitude, Phase, Real, Imaginary) for each image.
-6. Customize weights for Fourier Transform components using sliders.
-7. Select regions for output components (inner or outer) using customizable sliders or resize handles.
-8. Explore real-time mixing capabilities, observing the progress with the displayed progress bar.
-9. Cancel ongoing operations if needed and initiate new ones seamlessly.
-
-
-## Demo
-
-Curious to see the magic in action? Watch our demo video and experience the transformative power of our Mixer.
-
 ## Contributors
 
 Gratitude goes out to all team members for their valuable contributions to this project.
 
 <div align="left">
+    <a href="https://github.com/AhmeddEmad7">
+    <img src="https://github.com/AhmeddEmad7.png" width="100px" alt="@AhmeddEmad7">
+  </a>
   <a href="https://github.com/hazemzakariasaad">
     <img src="https://github.com/hazemzakariasaad.png" width="100px" alt="@hazemzakariasaad">
   </a>
   <a href="https://github.com/nourhan-ahmedd">
     <img src="https://github.com/nourhan-ahmedd.png" width="100px" alt="@nourhan-ahmedd">
-  </a>
-  <a href="https://github.com/AhmeddEmad7">
-    <img src="https://github.com/AhmeddEmad7.png" width="100px" alt="@AhmeddEmad7">
   </a>
   <a href="https://github.com/raghdaneiazyy6">
     <img src="https://github.com/raghdaneiazyy6.png" width="100px" alt="@raghdaneiazy6">
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# main_app.py
-
 import sys
 from os import path
-
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsPixmapItem, QApplication
 import cv2
-import numpy as np
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUiType
 from PyQt5.QtGui import QPixmap, QImage
-from imageDisplay import ImageDisplay
+from PyQt5.QtCore import Qt, QTimer
+from ImageDisplay import ImageDisplay
+import logging
+logging.basicConfig(filename='user.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "design.ui"))
 
@@ -20,99 +20,144 @@ class MainApp(QMainWindow, FORM_CLASS):
         QMainWindow.__init__(self)
         self.setupUi(self)
         self.setWindowTitle("Image Mixer")
-        self.verticalSlider_1.valueChanged.connect(self.on_changed)
-        self.verticalSlider_2.valueChanged.connect(self.on_changed)
-        self.verticalSlider_3.valueChanged.connect(self.on_changed)
-        self.verticalSlider_4.valueChanged.connect(self.on_changed)
-        self.labels = [self.label_2, self.label_3, self.label_4, self.label_5]
 
-        self.verticalSlider_1.setValue(0)
-        self.verticalSlider_2.setValue(0)
-        self.verticalSlider_3.setValue(0)
-        self.verticalSlider_4.setValue(0)
+        for slider in [self.verticalSlider_1, self.verticalSlider_2, self.verticalSlider_3, self.verticalSlider_4]:   #refactored
+            slider.valueChanged.connect(self.on_changed)
+
+        for combo_box in [self.comboBox_1, self.comboBox_2, self.comboBox_3, self.comboBox_4]:       #refactored
+            combo_box.currentIndexChanged.connect(self.on_changed)
+
+        self.combobox_list = [self.comboBox_1,self.comboBox_2,self.comboBox_3,self.comboBox_4]
+        self.combobox_list_index= [self.comboBox_1.currentIndex(),self.comboBox_2.currentIndex(),self.comboBox_3.currentIndex(),self.comboBox_4.currentIndex()]
+
+        for slider in [self.verticalSlider_1, self.verticalSlider_2, self.verticalSlider_3, self.verticalSlider_4]: #refactored
+            slider.setValue(0)
+
+        for button in [self.insideButton_1, self.insideButton_2, self.insideButton_3, self.insideButton_4]:
+            button.setChecked(True)
+        
+        self.newimage = None
         # Create instances of ImageDisplay for each label
-        self.image_display1 = ImageDisplay(self.imageLabel1, self.imageComponent1, self.imageComboBox1,self.label_2,self.comboBox_5,self.outputImage1,self.outputImage2)
-        self.image_display2 = ImageDisplay(self.imageLabel2, self.imageComponent2, self.imageComboBox2,self.label_3, self.comboBox_5,self.outputImage1,self.outputImage2)
-        self.image_display3 = ImageDisplay(self.imageLabel3, self.imageComponent3, self.imageComboBox3,self.label_4,self.comboBox_5 ,self.outputImage1,self.outputImage2)
-        self.image_display4 = ImageDisplay(self.imageLabel4, self.imageComponent4, self.imageComboBox4,self.label_5, self.comboBox_5,self.outputImage1,self.outputImage2)
-        # Assign the label and component attributes separately
-        self.image_display_label1 = self.image_display1.label
-        self.image_component1 = self.image_display1.image_component
+        self.image_display1 = ImageDisplay(self.imageLabel1, self.imageComponent1, self.imageComboBox1, self.comboBox_5, self.insideButton_1, 0)
+        self.image_display2 = ImageDisplay(self.imageLabel2, self.imageComponent2, self.imageComboBox2, self.comboBox_5, self.insideButton_2, 0)
+        self.image_display3 = ImageDisplay(self.imageLabel3, self.imageComponent3, self.imageComboBox3, self.comboBox_5, self.insideButton_3, -1)
+        self.image_display4 = ImageDisplay(self.imageLabel4, self.imageComponent4, self.imageComboBox4, self.comboBox_5, self.insideButton_4, 0)
 
-        self.newimage =None
-        self.image_display_label2 = self.image_display2.label
-        self.image_component2 = self.image_display2.image_component
+        self.progressBar.setValue(0)  # Set initial value of the progress bar
 
-        self.image_display_label3 = self.image_display3.label
-        self.image_component3 = self.image_display3.image_component
+        for label, image_display in zip([self.imageLabel1, self.imageLabel2, self.imageLabel3, self.imageLabel4], [self.image_display1, self.image_display2, self.image_display3, self.image_display4]):
+            label.mouseDoubleClickEvent = image_display.on_label_double_clicked
 
-        self.image_display_label4 = self.image_display4.label
-        self.image_component4 = self.image_display4.image_component
+        for combo_box, image_display in zip([self.imageComboBox1, self.imageComboBox2, self.imageComboBox3, self.imageComboBox4], [self.image_display1, self.image_display2, self.image_display3, self.image_display4]):
+            combo_box.currentIndexChanged.connect(image_display.handle_combobox_change)
 
+        for radiobutton, image_display in zip([self.insideButton_1, self.insideButton_2, self.insideButton_3, self.insideButton_4], [self.image_display1, self.image_display2, self.image_display3, self.image_display4]):
+            radiobutton.toggled.connect(image_display.ExtractRegion)
+            radiobutton.toggled.connect(self.on_changed)
 
-        # Connect the double-clicked signal to the corresponding on_label_double_clicked function
-        self.imageLabel1.mouseDoubleClickEvent = self.image_display1.on_label_double_clicked
-        self.imageLabel2.mouseDoubleClickEvent = self.image_display2.on_label_double_clicked
-        self.imageLabel3.mouseDoubleClickEvent = self.image_display3.on_label_double_clicked
-        self.imageLabel4.mouseDoubleClickEvent = self.image_display4.on_label_double_clicked
+        for radiobutton, image_display in zip([self.outsideButton_1, self.outsideButton_2, self.outsideButton_3, self.outsideButton_4], [self.image_display1, self.image_display2, self.image_display3, self.image_display4]):
+            radiobutton.toggled.connect(image_display.ExtractRegion)
+            radiobutton.toggled.connect(self.on_changed)
 
-        # Connect the combo box change signal to the handle_combobox_change function
-        self.imageComboBox1.currentIndexChanged.connect(self.image_display1.handle_combobox_change)
-        self.imageComboBox2.currentIndexChanged.connect(self.image_display2.handle_combobox_change)
-        self.imageComboBox3.currentIndexChanged.connect(self.image_display3.handle_combobox_change)
-        self.imageComboBox4.currentIndexChanged.connect(self.image_display4.handle_combobox_change)
         # Connect the combination signal to the combination function
         self.radioButton1.setChecked(True)
         self.radioButton1.toggled.connect(self.on_changed)
-        self.radioButton2.toggled.connect(self.changelabel)
-        self.pushButton.clicked.connect(lambda :self.press_Apply(self.newimage))
+        self.pushButton.clicked.connect(self.start_progress)
+        self.pushButton_2.clicked.connect(self.cancel_operation)
+        self.verticalSlider.valueChanged.connect(self.updateRectangle)
 
-    def changelabel(self):
-        if self.radioButton2.isChecked():
-            for label in self.labels:
-                label.setText("Real/Imaginary")
+    def start_progress(self):
+        self.progressBar.setValue(0)  # Set initial value of the progress bar
+        # Create a QTimer to simulate progress updates
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_progress)
+        self.timer.start(12)  # Adjust the interval (in milliseconds) as needed
+
+    def update_progress(self):
+        # Update the progress bar value
+        current_value = self.progressBar.value()
+        if current_value < 100:
+            self.progressBar.setValue(current_value + 1)  # Increment the progress value
         else:
-            for label in self.labels:
-                label.setText("Magnitude/Phase")
+            self.timer.stop()
+            self.press_Apply(self.newimage)
+            # Stop the timer when the progress reaches 100%
+
+    def cancel_operation(self):
+        if hasattr(self, 'timer') and self.timer.isActive():
+            self.timer.stop()  # Stop the timer if it is active
+        self.progressBar.setValue(0)
 
     def press_Apply(self,newimage):
-        
         index = self.comboBox_5.currentIndex()
-        
-        resized_image = cv2.resize(newimage, (1500, 500), interpolation=cv2.INTER_AREA)
-        
+        if index == 0:
+            logging.info(f'Output image will be displayed in output #1')
+        else: 
+            logging.info(f'Output image will be displayed in output #2')
+        newimage = cv2.add(newimage, 50)
+        resized_image = cv2.resize(newimage, (self.outputImage1.width(), self.outputImage1.height()), interpolation=cv2.INTER_AREA)
         height, width = resized_image.shape
         bytes_per_line = width
         q_image = QImage(resized_image.data, width, height, bytes_per_line, QImage.Format_Grayscale8)
-        pixmap = QPixmap.fromImage(q_image)
-        
+        scene = QGraphicsScene()
+        pixmap_item = QGraphicsPixmapItem(QPixmap.fromImage(q_image))
+        scene.addItem(pixmap_item)
+        logging.info('Rectangle size updated successfully.')
+        logging.info('Output image is displayed.')
 
         if index==0 :
-            scaled_pixmap = pixmap.scaled(self.outputImage1.size(), aspectRatioMode=Qt.KeepAspectRatio)
-            self.outputImage1.setPixmap(scaled_pixmap)
+            self.outputImage1.setScene(scene)
+            self.outputImage1.fitInView(pixmap_item, Qt.KeepAspectRatio)
         else:
-            scaled_pixmap = pixmap.scaled(self.outputImage2.size(), aspectRatioMode=Qt.KeepAspectRatio)
-            self.outputImage2.setPixmap(scaled_pixmap)
+            self.outputImage2.setScene(scene)
+            self.outputImage2.fitInView(pixmap_item, Qt.KeepAspectRatio)
 
     def on_changed(self):
-            # Fetch current indexes of other comboboxes    
-            slider_1 = self.verticalSlider_1.value()
-            slider_2 = self.verticalSlider_2.value()
-            slider_3 = self.verticalSlider_3.value()
-            slider_4 = self.verticalSlider_4.value()
-            
-            if self.radioButton1.isChecked():
-                index = 0
+        slider_values = [self.verticalSlider_1.value(), self.verticalSlider_2.value(), self.verticalSlider_3.value(), self.verticalSlider_4.value()]
+        if self.radioButton1.isChecked():
+            index = 0
+            component_labels = ["Magnitude", "Phase"]
+            logging.info(f'Magnitude/Phase mode selected')
+        else:
+            index = 1
+            component_labels = ["Real", "Imaginary"]
+            logging.info(f'Real/Imaginary mode selected')
+        
+        for i, (slider_value, combo_box) in enumerate(zip(slider_values, self.combobox_list)):
+            if combo_box.currentIndex() == 0:
+                logging.info(f'Slider {i + 1} value: {slider_value}, Manipulated image: {i + 1}, Component: {component_labels[0]}')
             else:
-                index = 1
-            
-    # Fetch current indexes of other comboboxes
-            self.newimage = ImageDisplay.combination(self.image_display1, self.image_display2, self.image_display3, self.image_display4,
-                                    index, slider_1, slider_2, slider_3, slider_4)
+                logging.info(f'Slider {i + 1} value: {slider_value}, Manipulated image: {i + 1}, Component: {component_labels[1]}')
+
+        for i, combo_box in enumerate(self.combobox_list):
+            combo_box.setItemText(0, component_labels[0])
+            combo_box.setItemText(1, component_labels[1])
+
+        indexes = [combo_box.currentIndex() for combo_box in [self.comboBox_1, self.comboBox_2, self.comboBox_3, self.comboBox_4]]
+        if self.imageLabel4.scene() is not None :
+            self.newimage = ImageDisplay.combination(self.image_display1, self.image_display2, self.image_display3, self.image_display4, index, *slider_values, indexes)
             self.newimage = cv2.normalize(self.newimage, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
+            self.newimage  = self.newimage .astype('uint8')
 
+    def updateRectangle(self):
+        max_width = self.imageComponent1.width()
+        max_height = self.imageComponent1.height()
+        self.verticalSlider.setMaximum(max_width)
+        value = self.verticalSlider.value()
+        if(value >= max_height):
+            self.image_display1.update_rect_size(value, max_height)
+            self.image_display2.update_rect_size(value, max_height)
+            self.image_display3.update_rect_size(value, max_height)
+            self.image_display4.update_rect_size(value, max_height)
+        else:
+            self.image_display1.update_rect_size(value, value)
+            self.image_display2.update_rect_size(value, value)
+            self.image_display3.update_rect_size(value, value)
+            self.image_display4.update_rect_size(value, value)
 
+        self.on_changed()
+    
 
 def main():
     app = QApplication(sys.argv)
